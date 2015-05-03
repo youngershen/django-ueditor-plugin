b78f7de458c0b16469b65f905d35e6735f6849fe
youngershen <younger.x.shen@gmail.com>
2015-05-03 12:09:38 +0800

    add admin model

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 
 from setuptools import setup, find_packages
 
-version = '0.11'
+version = '0.12'
 
 setup(name='django-ueditor-plugin',
       version=version,
--- ueditor/admin.py
+++ ueditor/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import UeditorFile
 
-# Register your models here.
+
+class UeditorFileAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(UeditorFile, UeditorFileAdmin)
